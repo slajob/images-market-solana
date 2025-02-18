71777d34e28a2d3bc27504a541828f93b54dfaaa
slajob <slawekjob174@gmail.com>
2025-02-18 22:52:43 +0100

    login/ logout implemente

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
 from typing import Optional
 import sqlalchemy
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sqlalchemy.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sqlalchemy.String(128), index=True, unique=True)
@@ -13,6 +15,13 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -23,3 +32,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
