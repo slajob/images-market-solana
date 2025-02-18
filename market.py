9f4640f14320e846f49565b2981f1e67f7127d6d
slajob <slawekjob174@gmail.com>
2025-02-18 22:21:00 +0100

    shell context added

--- market.py
+++ market.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+import sqlalchemy
+import sqlalchemy.orm as so
+from app.models import User, Post
+
+@app.shell_context_processor
+def maker_shell_context():
+    return {'sa': sqlalchemy, 'so': so, 'db': db, 'User': User, 'Post': Post}
