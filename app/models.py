128148a3907c2c39c3e246d87bd4683640ad494c
slajob <slawekjob174@gmail.com>
2025-02-18 22:04:34 +0100

    post table added, migrated and upgraded

--- app/models.py
+++ app/models.py
@@ -2,12 +2,24 @@ from typing import Optional
 import sqlalchemy
 import sqlalchemy.orm as so
 from app import db
+from datetime import datetime, timezone
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sqlalchemy.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sqlalchemy.String(128), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sqlalchemy.String(256))
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
 
     def __repr__(self):
         return '<User {}'.format(self.username)
+
+class Post(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sqlalchemy.String(256))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sqlalchemy.ForeignKey(User.id), index=True)
+    author: so.Mapped[User] = so.relationship(back_populates='posts')
+
+    def __repr(self):
+        return '<Post {}'.format(self.body)
