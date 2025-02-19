ba1177adf8c3af35d96470a7115816770251fd2d
slajob <slawekjob174@gmail.com>
2025-02-19 17:38:46 +0100

    custom errors, error_cat

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
+import logging
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,4 +14,14 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 
-from app import routes, models
+from app import routes, models, errors
+
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    file_handler = RotatingFileHandler('logs/solana-market.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('App started')
